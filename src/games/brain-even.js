23becbd4b4a330d31d11546df754a2eac53610c7
ivan samozsecond <i_dont_known@mail.ru>
2022-08-15 21:31:42 +0300

    Refactor brain-even for new engine

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,19 +1,11 @@
-#!/usr/bin/env node
-import readline from 'readline-sync';
-import { getUserName, sayHi } from '../src/cli.js';
-import { startGame, getRandomNum } from '../src/index.js';
+import { getRandomNumber } from '../utils.js';
 
-console.log('Welcome to the Brain Games!');
-const name = getUserName();
-sayHi(name);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+export const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const getNumForTask = () => getRandomNum(50, 1);
+export const generateRound = () => {
+  const number = getRandomNumber(50, 1);
 
-const getRightAnswer = (num) => ((num % 2 === 0) ? 'yes' : 'no');
+  const isEven = (num) => ((num % 2 === 0) ? 'yes' : 'no');
 
-const getAnswer = () => readline.question('Your answer: ');
-
-const getQuestion = (num) => num;
-
-startGame(getNumForTask, getQuestion, getRightAnswer, getAnswer, name);
+  return [isEven(number), number];
+};
