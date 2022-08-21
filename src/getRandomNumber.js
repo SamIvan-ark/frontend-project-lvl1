c780ea4521041152cb7904e578efa7e462cf4284
ivan samozsecond <i_dont_known@mail.ru>
2022-08-21 12:39:09 +0300

    Refactor 2, step 1 Deleted unusual abstraction at utils.js utils.js renamed to getRandomNumber.js

---
+++ src/getRandomNumber.js
@@ -0,0 +1,5 @@
+export default (max, min = 1) => {
+  const a = Math.ceil(min);
+  const b = Math.floor(max);
+  return Math.floor(Math.random() * (b - a + 1)) + a;
+};
--- src/utils.js
+++
@@ -1,9 +0,0 @@
-import readline from 'readline-sync';
-
-export const askQuestionAndGetAnswer = (question) => readline.question(question);
-
-export const getRandomNumber = (max, min = 1) => {
-  const a = Math.ceil(min);
-  const b = Math.floor(max);
-  return Math.floor(Math.random() * (b - a + 1)) + a;
-};
