c48d05884510d7d098badf1c2307620fe9b96d74
ivan samozsecond <i_dont_known@mail.ru>
2022-08-23 19:47:14 +0300

    Fix semantic error at brain-even (refactor 3, step 2)
    Predicate-function isEven now is return bool.

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -3,11 +3,11 @@ import runEngine from '../index.js';
 
 const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (num) => ((num % 2 === 0) ? 'yes' : 'no');
+const isEven = (num) => num % 2 === 0;
 
 const generateRound = () => {
   const queston = getRandomNumber(1, 50);
-  const answer = isEven(queston);
+  const answer = isEven(queston) ? 'yes' : 'no';
 
   return [answer, queston];
 };
